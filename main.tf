5532b7e6864a9ae12e5bf2d8ee5cd7106d10ca8d
Matt Rigby <mrigby83@gmail.com>
2021-03-19 20:41:09 +0000

    testing a push to test-branch

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "mrigby-cloud"
+    organization = "mrigby-cloudf"
 
     workspaces {
       name = "playground"
