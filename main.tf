d1e726638759925ca7aa14ec238fbf3d5d1ec6f0
Matt Rigby <mrigby83@gmail.com>
2021-03-19 19:58:09 +0000

    added terraform main.tf file

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = "~> 0.14"
+
+  backend "remote" {
+    organization = "mrigby-cloud"
+
+    workspaces {
+      name = "playground"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "aws_key" {
+  key_name   = "aws_id_rsa"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdzG50ljkKy+BVkdLy74Utd0sjGFPGqAEqVgeGzGBE2MUgzBz5gz9X0BELabpvKDOqXmBsD2xKT3jWmvBMelS+lMNT0iKOIA868PPtq1DOpQ3/fYhu06KCwXQ5cI76Ll9fEWMfgC0oyxjBTSIGcjaTIm9rgKba3HTedB8uU+sEhuVUDox0a4EF7f6rsPqIBWsEFLR+wVldMcajrPAXpo+5RcTeO8j399hK2x0ceAablrP5dp9dpcexOHOqNG04VcQH0Ri4dE3TX/GTZPh0Z7WeZaLyvTGsQVgreali5RahM5twD1ZALXG9Y7vNoD9VQu3Rkfx5Vmp/kusC3nsLEEiD matt@G02UKXN06825"
+}
+
+resource "aws_security_group" "stock-check-sec-group" {
+  name = "stock-check-sec-group"
+  ingress {
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "stock-check" {
+  ami           = "ami-0915bcb5fa77e4892"
+  instance_type = "t2.micro"
+  key_name = "aws_id_rsa"
+  vpc_security_group_ids = [aws_security_group.stock-check-sec-group.id]
+
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}
